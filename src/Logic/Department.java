c9c604b35d83b100437b40ead11357aff7ce318f
raghad <raghadalesafi2005@gmail.com>
2025-05-11 09:56:46 +0300

    first commit with Raghad

--- src/Logic/Department.java
+++ src/Logic/Department.java
@@ -3,11 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Logic;
-
-/**
- *
- * @author zainab
- */
+//Raghad did something
 public class Department {
     private int id;
     private String name;
